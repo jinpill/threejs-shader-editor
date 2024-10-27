d2b318acb1fad4984f2c59fb4eddd1dcf6831abe
jinpill <hello@jinpill.dev>
2024-10-27 14:51:12 +0900

    feat: scss 샘플 코드 추가

--- src/components/SampleButton/index.tsx
+++ src/components/SampleButton/index.tsx
@@ -1,10 +1,17 @@
+import style from "./style.module.scss";
+
 type SampleButtonProps = {
   size: "small" | "medium" | "large";
   children?: React.ReactNode;
 };
 
 const SampleButton = (props: SampleButtonProps) => {
-  return <button>{props.children}</button>;
+  return (
+    <button className={style.sampleButton}>
+      {props.children}
+      <div />
+    </button>
+  );
 };
 
 export default SampleButton;

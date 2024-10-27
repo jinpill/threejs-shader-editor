051370bd8c4363e24f74ca9cece79f7b95ff3fff
jubi <joobe1031@gmail.com>
2024-10-27 18:06:50 +0900

    feat: editor 컴포넌트 만들기

---
+++ src/components/Editor/Editor.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import Editor from '.';
+
+const meta = {
+  title: 'Reusable/Editor',
+  component: Editor,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onChange: fn() },
+} satisfies Meta<typeof Editor>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};

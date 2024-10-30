cf73c6b1aa7f4be8c035aa530bc3dabc42928fbc
jinpill <hello@jinpill.dev>
2024-10-30 23:55:21 +0900

    feat: useDragAndDrop 훅 추가

---
+++ src/hooks/useDragAndDrop.ts
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import useStateRef from "@/hooks/useStateRef";
+
+type DropEventHandler = (files: File[], event: DragEvent) => void;
+
+const useDragAndDrop = <E extends HTMLElement>(onDrop: DropEventHandler) => {
+  const elementRef = useRef<E>(null);
+  const [dragging, setDragging] = useState(false);
+  const onDropRef = useStateRef(onDrop);
+
+  useEffect(() => {
+    const element = elementRef.current;
+    if (!element) return;
+
+    const handleDragEnter = () => {
+      setDragging(true);
+    };
+
+    const handleDragLeave = () => {
+      setDragging(false);
+    };
+
+    const handleDragOver = (e: DragEvent) => {
+      e.preventDefault();
+    };
+
+    const handleDrop = (e: DragEvent) => {
+      e.preventDefault();
+      setDragging(false);
+
+      const files = Array.from(e.dataTransfer?.files ?? []);
+      if (files.length > 0) onDropRef.current(files, e);
+    };
+
+    element.addEventListener("dragenter", handleDragEnter);
+    element.addEventListener("dragleave", handleDragLeave);
+    element.addEventListener("dragover", handleDragOver);
+    element.addEventListener("drop", handleDrop);
+
+    return () => {
+      element.removeEventListener("dragenter", handleDragEnter);
+      element.removeEventListener("dragleave", handleDragLeave);
+      element.removeEventListener("dragover", handleDragOver);
+      element.removeEventListener("drop", handleDrop);
+    };
+  }, [onDropRef]);
+
+  return {
+    ref: elementRef,
+    dragging,
+  };
+};
+
+export default useDragAndDrop;

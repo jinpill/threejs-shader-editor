03e0f5e7702156182b0f8c301c50b56faa9adc39
jinpill <hello@jinpill.dev>
2024-11-14 01:16:49 +0900

    refactor: 테스트 코드 제거

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,20 +1,9 @@
-"use client";
-
 import SampleButton from "@/components/SampleButton";
-import Editor from "@/components/Editor";
-import { useState } from "react";
 
 const EditorPage = () => {
-  const [fragmentShader, setFragmentShader] = useState("");
-
   return (
     <div>
       <h1>Editor</h1>
-      <Editor
-        label="Fragment Shader"
-        value={fragmentShader}
-        onChange={setFragmentShader}
-      />
       <SampleButton size="medium">Sample Button</SampleButton>
     </div>
   );

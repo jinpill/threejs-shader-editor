2a2e1652b9516559a735f477a7f7bab3020c1bde
jinpill <hello@jinpill.dev>
2024-12-07 00:38:08 +0900

    feat: ToolPanel 컴포넌트의 차일드 컴포넌트 추가

--- src/components/ToolPanel/index.tsx
+++ src/components/ToolPanel/index.tsx
@@ -1,16 +1,31 @@
+import classNames from "classnames";
 import { Paper } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+
+import ToolPanelSection from "./Section";
+import ToolPanelLabel from "./Label";
+import ToolPanelButtons from "./Buttons";
+import ToolPanelButton from "./Button";
+
 import style from "./style.module.scss";
 
 export type ToolPanelProps = {
   title: string;
   onClose?: () => void;
+
+  className?: string;
   children?: React.ReactNode;
+  style?: React.CSSProperties;
 };
 
+/**
+ * - `--tool-panel-width` ToolPanel의 너비를 지정 (default: 400px)
+ * - `--tool-panel-height` ToolPanel의 높이를 지정 (default: auto)
+ * - `--tool-panel-max-height` ToolPanel의 최대 높이를 지정 (default: 640px)
+ */
 const ToolPanel = (props: ToolPanelProps) => {
   return (
-    <Paper className={style.toolPanel}>
+    <Paper className={classNames(style.toolPanel, props.className)} style={props.style}>
       <header className={style.header}>
         <div className={style.title}>{props.title}</div>
 
@@ -19,9 +34,15 @@ const ToolPanel = (props: ToolPanelProps) => {
         </button>
       </header>
 
-      <div className={style.contents}>{props.children}</div>
+      <div className={style.contents}>
+        <div>{props.children}</div>
+      </div>
     </Paper>
   );
 };
 
+ToolPanel.Section = ToolPanelSection;
+ToolPanel.Label = ToolPanelLabel;
+ToolPanel.Buttons = ToolPanelButtons;
+ToolPanel.Button = ToolPanelButton;
 export default ToolPanel;

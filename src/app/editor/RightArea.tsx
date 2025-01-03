e6afe573e4e4b3bcae0cda718f3abc74d3b7f859
jinpill <hello@jinpill.dev>
2025-01-03 23:02:17 +0900

    refactor: RightArea 컴포넌트 분리

---
+++ src/app/editor/RightArea.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Paper } from "@mui/material";
+import Editor from "@/components/Editor";
+
+import style from "./style.module.scss";
+
+type RightAreaProps = {
+  vertexShader: string;
+  fragmentShader: string;
+  onChangeVertexShader: (value: string) => void;
+  onChangeFragmentShader: (value: string) => void;
+};
+
+const RightArea = (props: RightAreaProps) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <Paper
+      elevation={4}
+      className={classNames(style.rightArea, {
+        [style.collapsed]: isCollapsed,
+      })}
+    >
+      {/* 접기/펼치기 버튼 */}
+      <div className={style.collapseButton} onClick={() => setIsCollapsed(!isCollapsed)}>
+        {isCollapsed ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+      </div>
+
+      {/* 텍스트 영역 */}
+      {!isCollapsed && (
+        <div className={style.textAreaContainer}>
+          <Editor
+            className={style.editor}
+            label="Vertex Shader"
+            value={props.vertexShader}
+            onChange={props.onChangeVertexShader}
+            placeholder="예시코드를 입력해주세요..."
+          />
+          <Editor
+            className={style.editor}
+            label="Fragment Shader"
+            value={props.fragmentShader}
+            onChange={props.onChangeFragmentShader}
+            placeholder="예시코드를 입력해주세요..."
+          />
+        </div>
+      )}
+    </Paper>
+  );
+};
+
+export default RightArea;

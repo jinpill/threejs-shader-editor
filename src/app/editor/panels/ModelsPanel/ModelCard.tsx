c2f797b15278d5fc89cf3f86c1524af2b59f1a59
jubi <joobe1031@gmail.com>
2025-01-06 22:21:07 +0900

    feat: 프리셋 스타일 구현

--- src/app/editor/panels/ModelsPanel/ModelCard.tsx
+++ src/app/editor/panels/ModelsPanel/ModelCard.tsx
@@ -1,15 +1,39 @@
 import style from "./style.module.scss";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
 type ModelCardProps = {
   name: string;
+  details: string;
   onClick: () => void;
 };
 
 const ModelCard = (props: ModelCardProps) => {
   return (
-    <div className={style.modelCard} onClick={props.onClick}>
-      {props.name}
-    </div>
+    <CardActionArea>
+      <Card className={style.modelCard} onClick={props.onClick}>
+        <CardMedia
+          className={style.ImgCard}
+          component="img"
+          image="/bunny.svg"
+          alt="Bunny"
+        />
+        <Box>
+          <CardContent className={style.CardCon}>
+            <Typography className={style.TitleCard} component="div">
+              {props.name}
+            </Typography>
+            <Typography className={style.DecCard} component="div">
+              {props.details}
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </CardActionArea>
   );
 };
 

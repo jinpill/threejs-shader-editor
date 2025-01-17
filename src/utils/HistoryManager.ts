a0a31870d892e3491a0150c340c786136f29907c
jinpill <hello@jinpill.dev>
2025-01-17 23:47:40 +0900

    feat: HistoryManager 클래스 추가

---
+++ src/utils/HistoryManager.ts
@@ -0,0 +1,72 @@
+const TIMEOUT_DURATION = 1000;
+
+export class HistoryManager {
+  private history: string[] = [];
+  private currentIndex = -1;
+  private timeoutId: number | null = null;
+  private readonly maxLength: number;
+
+  constructor(maxLength: number) {
+    this.maxLength = maxLength;
+  }
+
+  public push = (state: string) => {
+    this.history = this.history.slice(0, this.currentIndex + 1);
+
+    if (typeof this.timeoutId === "number") {
+      this.history[this.currentIndex] = state;
+      window.clearTimeout(this.timeoutId);
+    } else {
+      if (this.history.length === this.maxLength) {
+        this.history.shift();
+      }
+
+      this.history.push(state);
+      this.currentIndex = this.history.length - 1;
+    }
+
+    this.timeoutId = window.setTimeout(() => {
+      this.timeoutId = null;
+    }, TIMEOUT_DURATION);
+  };
+
+  private clearTimeout = () => {
+    if (typeof this.timeoutId !== "number") return;
+    window.clearTimeout(this.timeoutId);
+    this.timeoutId = null;
+  };
+
+  public get isUndoAvailable() {
+    return this.currentIndex > 0;
+  }
+
+  public get isRedoAvailable() {
+    return this.currentIndex < this.history.length - 1;
+  }
+
+  public get current() {
+    return this.history[this.currentIndex] ?? "";
+  }
+
+  public undo = () => {
+    this.clearTimeout();
+
+    if (this.isUndoAvailable) {
+      this.currentIndex--;
+      return this.current;
+    } else {
+      return "";
+    }
+  };
+
+  public redo = () => {
+    this.clearTimeout();
+
+    if (this.isRedoAvailable) {
+      this.currentIndex++;
+      return this.current;
+    } else {
+      return "";
+    }
+  };
+}

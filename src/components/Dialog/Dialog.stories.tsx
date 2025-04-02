02acbe8587f0a5c29f5f6d2fa6136676b3a43e1e
jinpill <hello@jinpill.dev>
2025-04-03 00:07:48 +0900

    feat: 다크테마 스타일 추가

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -212,7 +212,7 @@ export const Warning: Story = {
       },
       {
         icon: "check",
-        type: "primary",
+        type: "danger",
         label: "확인",
         value: true,
       },
@@ -224,6 +224,282 @@ export const Warning: Story = {
 export const Error: Story = {
   ...getContainerConfigs({
     minHeight: "500px",
+  }),
+  render: (args) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [props, setProps] = useState(args as DialogProps<boolean>);
+
+    const confirm = () => {
+      const { promise, resolve } = Promise.withResolvers<boolean>();
+      setIsVisible(true);
+      setProps((prev) => ({
+        ...prev,
+        callback: (value: boolean) => {
+          resolve(value);
+          setIsVisible(false);
+        },
+      }));
+      return promise;
+    };
+
+    const handleClick = async () => {
+      const value = await confirm();
+      console.log("value:", value);
+    };
+
+    useEffect(() => {
+      setProps((prev) => ({
+        ...(args as DialogProps<boolean>),
+        callback: prev.callback,
+      }));
+    }, [args]);
+
+    return (
+      <>
+        <Button label="다이얼로그 열기" onClick={handleClick} />
+
+        <MountAnimation isVisible={isVisible}>
+          <Dialog {...props} />
+        </MountAnimation>
+      </>
+    );
+  },
+  args: {
+    type: "error",
+    title: "Lorem Ipsum Dolor Sit Amet",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    defaultValue: false,
+    defaultFocus: 0,
+    buttons: [
+      {
+        icon: "close",
+        type: "secondary",
+        label: "취소",
+        value: false,
+      },
+      {
+        icon: "check",
+        type: "danger",
+        label: "확인",
+        value: true,
+      },
+    ],
+    callback: () => {},
+  },
+};
+
+export const InfoDark: Story = {
+  ...getContainerConfigs({
+    minHeight: "500px",
+    theme: "dark",
+  }),
+  render: (args) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [props, setProps] = useState(args as DialogProps<boolean>);
+
+    const confirm = () => {
+      const { promise, resolve } = Promise.withResolvers<boolean>();
+      setIsVisible(true);
+      setProps((prev) => ({
+        ...prev,
+        callback: (value: boolean) => {
+          resolve(value);
+          setIsVisible(false);
+        },
+      }));
+      return promise;
+    };
+
+    const handleClick = async () => {
+      const value = await confirm();
+      console.log("value:", value);
+    };
+
+    useEffect(() => {
+      setProps((prev) => ({
+        ...(args as DialogProps<boolean>),
+        callback: prev.callback,
+      }));
+    }, [args]);
+
+    return (
+      <>
+        <Button label="다이얼로그 열기" onClick={handleClick} />
+
+        <MountAnimation isVisible={isVisible}>
+          <Dialog {...props} />
+        </MountAnimation>
+      </>
+    );
+  },
+  args: {
+    type: "info",
+    title: "Lorem Ipsum Dolor Sit Amet",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    defaultValue: false,
+    defaultFocus: 1,
+    buttons: [
+      {
+        icon: "close",
+        type: "secondary",
+        label: "취소",
+        value: false,
+      },
+      {
+        icon: "check",
+        type: "primary",
+        label: "확인",
+        value: true,
+      },
+    ],
+    callback: () => {},
+  },
+};
+
+export const SuccessDark: Story = {
+  ...getContainerConfigs({
+    minHeight: "500px",
+    theme: "dark",
+  }),
+  render: (args) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [props, setProps] = useState(args as DialogProps<boolean>);
+
+    const confirm = () => {
+      const { promise, resolve } = Promise.withResolvers<boolean>();
+      setIsVisible(true);
+      setProps((prev) => ({
+        ...prev,
+        callback: (value: boolean) => {
+          resolve(value);
+          setIsVisible(false);
+        },
+      }));
+      return promise;
+    };
+
+    const handleClick = async () => {
+      const value = await confirm();
+      console.log("value:", value);
+    };
+
+    useEffect(() => {
+      setProps((prev) => ({
+        ...(args as DialogProps<boolean>),
+        callback: prev.callback,
+      }));
+    }, [args]);
+
+    return (
+      <>
+        <Button label="다이얼로그 열기" onClick={handleClick} />
+
+        <MountAnimation isVisible={isVisible}>
+          <Dialog {...props} />
+        </MountAnimation>
+      </>
+    );
+  },
+  args: {
+    type: "success",
+    title: "Lorem Ipsum Dolor Sit Amet",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    defaultValue: false,
+    defaultFocus: 1,
+    buttons: [
+      {
+        icon: "close",
+        type: "secondary",
+        label: "취소",
+        value: false,
+      },
+      {
+        icon: "check",
+        type: "primary",
+        label: "확인",
+        value: true,
+      },
+    ],
+    callback: () => {},
+  },
+};
+
+export const WarningDark: Story = {
+  ...getContainerConfigs({
+    minHeight: "500px",
+    theme: "dark",
+  }),
+  render: (args) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [props, setProps] = useState(args as DialogProps<boolean>);
+
+    const confirm = () => {
+      const { promise, resolve } = Promise.withResolvers<boolean>();
+      setIsVisible(true);
+      setProps((prev) => ({
+        ...prev,
+        callback: (value: boolean) => {
+          resolve(value);
+          setIsVisible(false);
+        },
+      }));
+      return promise;
+    };
+
+    const handleClick = async () => {
+      const value = await confirm();
+      console.log("value:", value);
+    };
+
+    useEffect(() => {
+      setProps((prev) => ({
+        ...(args as DialogProps<boolean>),
+        callback: prev.callback,
+      }));
+    }, [args]);
+
+    return (
+      <>
+        <Button label="다이얼로그 열기" onClick={handleClick} />
+
+        <MountAnimation isVisible={isVisible}>
+          <Dialog {...props} />
+        </MountAnimation>
+      </>
+    );
+  },
+  args: {
+    type: "warning",
+    title: "Lorem Ipsum Dolor Sit Amet",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    defaultValue: false,
+    defaultFocus: 0,
+    buttons: [
+      {
+        icon: "close",
+        type: "secondary",
+        label: "취소",
+        value: false,
+      },
+      {
+        icon: "check",
+        type: "primary",
+        label: "확인",
+        value: true,
+      },
+    ],
+    callback: () => {},
+  },
+};
+
+export const ErrorDark: Story = {
+  ...getContainerConfigs({
+    minHeight: "500px",
+    theme: "dark",
   }),
   render: (args) => {
     const [isVisible, setIsVisible] = useState(false);
